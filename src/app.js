00dce911d94aa5747e96fc0e3e606eed7f0c7486
lboulle <lucas.figueiredo@ammovarejo.com.br>
2020-12-05 14:08:00 -0300

    Add all functionalities

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +10,69 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories) 
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const { id } = request.params
+
+  const repository = repositories.find(repository => repository.id === id)
+
+  if(!repository) {
+    response.status(400).json({message: 'Repositório não encontrado!'})
+  }
+
+  repository.title = title || repository.title
+  repository.url = url || repository.url
+  repository.techs = techs || repository.techs
+
+  return response.json(repository) 
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.map(repository => repository.id).indexOf(id);
+
+  if(index < 0) {
+    response.status(400).json({message: 'Repositório não encontrado!'})
+  }
+
+  repositories.splice(index, 1)
+
+  return response.status(204).json();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if(!repository) {
+    response.status(400).json({message: 'Repositório não encontrado!'})
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
